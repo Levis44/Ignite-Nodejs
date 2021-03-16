fe7be44c45872ac24e310a6a8cd281ef1ee2ae96
Levi <levi.ciarrocchi@gmail.com>
2021-03-16 19:39:29 -0300

    Criando API

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ mesage: "Opa" });
-});
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3000, () => console.log("Server Running"));
